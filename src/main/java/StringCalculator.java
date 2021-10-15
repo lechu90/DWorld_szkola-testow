c5fff0c96bfd02c16902a2851b5cbc10e7bc1b3a
Leszek Rossa <leszek.rossa@gmail.com>
2021-10-15 20:31:38 +0200

    initial commit

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class StringCalculator {
+    int add(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        } else {
+            String[] numbers = input.split(",");
+            int result = 0;
+            for (String number : numbers) {
+                result += getIntFrom(number);
+            }
+            return result;
+        }
+    }
+
+    private int getIntFrom(String number) {
+        return Integer.parseInt(number);
+    }
+}
