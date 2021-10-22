559ac2c2ad7c9e46633ee60c3363ea1b74e6433c
Leszek Rossa <leszek.rossa@gmail.com>
2021-10-22 21:09:23 +0200

    Moduł 2 - zadanie domowe - część 1
    - dodatkowe testy - bad practice proofs

--- src/test/java/VatServiceTest.java
+++ src/test/java/VatServiceTest.java
@@ -1,11 +1,16 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 class VatServiceTest {
     private VatService vatService;
@@ -69,9 +74,38 @@ class VatServiceTest {
         });
     }
 
-    // TODO: Pitfall #4 - .round(mathContext) - add test proof
-    // TODO: assertThat(grossPrice).isEqualTo(new BigDecimal("123.00")); // FAIL
-    // TODO: assertThat(grossPrice).isEqualTo(new BigDecimal("12.00")); // OK
+    // explanation: https://blogs.oracle.com/javamagazine/post/four-common-pitfalls-of-the-bigdecimal-class-and-how-to-avoid-them
+    @ParameterizedTest
+    @CsvSource({"2021.13,2485.9899", "21.50,26.445", "76767.66,94424.2218"})
+    void testAsBigDecimalBadPracticeProof_WhenWantToSetPrecisionOfFractionPartOfBigDecimal_ShouldNotUseRoundWithMathContext(String priceNetto, String expectedBrutto) {
+        BigDecimal vat = new BigDecimal("0.23");
+        BigDecimal netto = new BigDecimal(priceNetto);
+        MathContext mathContext = new MathContext(4);
+
+        BigDecimal brutto = netto.multiply(vat.add(BigDecimal.ONE)).round(mathContext);
+
+        assertSoftly(
+                softAssertions -> {
+                    softAssertions.assertThat(brutto).isNotEqualByComparingTo(new BigDecimal(expectedBrutto));
+                    softAssertions.assertThat(brutto.toPlainString()).isNotEqualTo(new BigDecimal(expectedBrutto).toPlainString()); // only for better human readable of proof
+                }
+        );
+    }
+
+    // explanation: https://blogs.oracle.com/javamagazine/post/four-common-pitfalls-of-the-bigdecimal-class-and-how-to-avoid-them
+    @ParameterizedTest
+    @CsvSource({"100.00,100", "10.0,10.00"})
+    void testAsBigDecimalBadPracticeProof_WhenWantToCompareNumbersByValueWithoutFormattingCheck_ShouldNotUseIsEqualTo(String numberInFirstPrecision, String numberInSecondPrecision) {
+        BigDecimal firstNumber = new BigDecimal(numberInFirstPrecision);
+        BigDecimal secondNumber = new BigDecimal(numberInSecondPrecision);
+
+        // boolean badPractice = firstNumber.equals(secondNumber);
+        assertThat(firstNumber).isNotEqualTo(secondNumber);
+        // boolean goodPractice = firstNumber.compareTo(secondNumber);
+        assertThat(firstNumber).isEqualByComparingTo(secondNumber);
+    }
+
+    // TODO: move to class BigDecimalBadPracticeProofTest
 
     private Product generateProductWithPrice(String price) {
         return new Product(UUID.randomUUID(), new BigDecimal(price));
