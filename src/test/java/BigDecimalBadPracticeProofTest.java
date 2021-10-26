281bb62ecde943318a14dd86e366e0203d23f44b
Leszek Rossa <leszek.rossa@gmail.com>
2021-10-26 15:02:14 +0200

    Moduł 2 - zadanie domowe - część 1
    - test bad practice BigDecimal - zaokrąglanie - dodano good practice + lepsze wydzielenie sekcji "when/act"

--- src/test/java/BigDecimalBadPracticeProofTest.java
+++ src/test/java/BigDecimalBadPracticeProofTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.math.RoundingMode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -15,18 +16,24 @@ class BigDecimalBadPracticeProofTest {
 
     // explanation: https://blogs.oracle.com/javamagazine/post/four-common-pitfalls-of-the-bigdecimal-class-and-how-to-avoid-them
     @ParameterizedTest
-    @CsvSource({"2021.13,2485.9899", "21.50,26.445", "76767.66,94424.2218"})
+    @CsvSource({"2021.13,2485.9899", "21.50,26.4450", "76767.66,94424.2218"})
     void testAsBigDecimalBadPracticeProof_WhenWantToSetPrecisionOfFractionPartOfBigDecimal_ShouldNotUseRoundWithMathContext(String priceNetto, String expectedBrutto) {
         BigDecimal vat = new BigDecimal("0.23");
         BigDecimal netto = new BigDecimal(priceNetto);
         MathContext mathContext = new MathContext(4);
+        BigDecimal brutto = netto.multiply(vat.add(BigDecimal.ONE));
 
-        BigDecimal brutto = netto.multiply(vat.add(BigDecimal.ONE)).round(mathContext);
+        // bad practice: .round(mathContext);
+        BigDecimal bruttoRoundedBad = brutto.round(mathContext);
+        // good practice: use .setScale(4, RoundingMode.HALF_UP);
+        BigDecimal bruttoRoundedGood = brutto.setScale(4, RoundingMode.HALF_UP);
 
         assertSoftly(
                 softAssertions -> {
-                    softAssertions.assertThat(brutto).isNotEqualByComparingTo(new BigDecimal(expectedBrutto));
-                    softAssertions.assertThat(brutto.toPlainString()).isNotEqualTo(new BigDecimal(expectedBrutto).toPlainString()); // only for better human readable of proof
+                    softAssertions.assertThat(bruttoRoundedBad).isNotEqualByComparingTo(new BigDecimal(expectedBrutto));
+                    softAssertions.assertThat(bruttoRoundedGood).isEqualByComparingTo(new BigDecimal(expectedBrutto));
+                    softAssertions.assertThat(bruttoRoundedBad.toPlainString()).isNotEqualTo(new BigDecimal(expectedBrutto).toPlainString()); // only for better human readable of proof
+                    softAssertions.assertThat(bruttoRoundedGood.toPlainString()).isEqualTo(new BigDecimal(expectedBrutto).toPlainString());
                 }
         );
     }
